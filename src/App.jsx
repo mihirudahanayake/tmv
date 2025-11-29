71733d6ba50de34760bb654871064961a2098f10
mihirudahanayake <mihirudahanayake@gmail.com>
2025-11-29 11:09:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Homepage from './pages/Homepage.jsx';
 import Home from './pages/Home.jsx';
@@ -10,7 +10,7 @@ import WorkList from './pages/WorkList.jsx';
 
 function App() {
   return (
-    <Router basename="/tmv">
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
