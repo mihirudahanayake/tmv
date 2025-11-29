cf134683c392bfefd46d2caa3a504eb9147b4338
mihirudahanayake <mihirudahanayake@gmail.com>
2025-11-29 10:55:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Homepage from './pages/Homepage.jsx'; // admin dashboard
-import Home from './pages/Home.jsx';         // user home (welcome)
+import Homepage from './pages/Homepage.jsx';
+import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
 import CreateUser from './pages/CreateUser.jsx';
@@ -10,17 +10,12 @@ import WorkList from './pages/WorkList.jsx';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/tmv">
       <Routes>
-        {/* Auth */}
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
-        {/* Role-based destinations */}
-        <Route path="/home" element={<Home />} />           {/* user */}
-        <Route path="/admin-home" element={<Homepage />} /> {/* admin */}
-
-        {/* Existing admin features (could be protected later) */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/admin-home" element={<Homepage />} />
         <Route path="/create-user" element={<CreateUser />} />
         <Route path="/assign-work" element={<AssignWork />} />
         <Route path="/work-list" element={<WorkList />} />
