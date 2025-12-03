088bfd15bd3884a86daad75a42e8e755b9aba23b
mihirudahanayake <mihirudahanayake@gmail.com>
2025-12-03 14:16:24 +0530

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,24 @@
 /* public/firebase-messaging-sw.js */
-importScripts('https://www.gstatic.com/firebasejs/10.12.3/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.3/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyCKPrE1B2ikGlnr6PmTq4JVLlBDlOe50Co",
-  authDomain: "tmv1-7fc4f.firebaseapp.com",
   projectId: "tmv1-7fc4f",
-  storageBucket: "tmv1-7fc4f.firebasestorage.app",
   messagingSenderId: "377085834114",
-  appId: "1:377085834114:web:aeb2ca19e51abe8fc5959b",
-  measurementId: "G-8J8CDN3QFF"
+  appId: "1:377085834114:web:aeb2ca19e51abe8fc5959b"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  const { title, body } = payload.notification || {};
-  self.registration.showNotification(title || 'TMV', {
-    body: body || 'You have a new task.',
-    icon: '/icon-192.png'
-  });
+  console.log('[firebase-messaging-sw.js] background message ', payload);
+
+  const notificationTitle = payload.notification?.title || 'Task update';
+  const notificationOptions = {
+    body: payload.notification?.body || '',
+    icon: '/icon-192.png'   // put an icon image in public/
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
